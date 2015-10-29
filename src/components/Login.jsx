da75b5d5d93868cf24a5050cba9de5867946f821
Miguel Julio <miguel.julio.developer@gmail.com>
2015-10-29 13:03:15 -0400

    adding react-router

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default React.createClass({
+  render: function(){
+    return (
+      <div className="login-page">
+        <h3>Login please:</h3>
+      </div>
+    )
+  }
+})
---
+++ src/components/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default React.createClass({
+  render: function(){
+    return (
+      <div className="products-page">
+        <h3>Product List:</h3>
+      </div>
+    )
+  }
+})
