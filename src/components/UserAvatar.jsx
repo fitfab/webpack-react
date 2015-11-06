0ee9251db16496bfd93857ee1e6a50c1ee1de72a
Miguel Julio <miguel.julio.developer@gmail.com>
2015-11-06 09:07:38 -0500

    modified z-index and other for the tooltip

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default React.createClass({
   render: function() {
     let { secureUser, user, left } = this.props;
-    let userAvatar = secureUser? 'tooltip active transit' : 'tooltip';
+    let userAvatar = secureUser? 'tooltip active transit' : 'tooltip transit';
     console.log(userAvatar)
     return (
       <div className={userAvatar}>
