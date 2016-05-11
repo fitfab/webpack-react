ea633d75dbd723fcfc014c7bfec0a25609876a7a
fitfab <fitfab@yahoo.com>
2016-05-11 17:15:50 -0400

    working with child processes

---
+++ childProcess.js
@@ -0,0 +1,23 @@
+var childProcess = require('child_process');
+
+/*childProcess.exec('node -v', function ( error, stdout, stderr ) {
+    console.log('stdout: ', stdout);
+    console.log('stderr: ', stderr);
+    if ( error !== null ) {
+        console.log('exec error: ', error);
+    }
+})*/
+
+var child = childProcess.exec('node -more');
+
+child.stdout.on('data', function ( data ) {
+    console.log('stdout: ', data);
+});
+
+child.stderr.on('data', function ( data ) {
+    console.log('stderr: ', data);
+});
+
+child.on('close', function ( code ) {
+    console.log('Closing code: ', code);
+})
